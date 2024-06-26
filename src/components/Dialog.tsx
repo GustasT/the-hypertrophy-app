18d97b3ac83f232ca19bad405cac7db9256af835
Gustas Tutinas <102315449+GustasT@users.noreply.github.com>
2024-06-26 20:41:31 +0200

    attempt to fix dialog

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -30,20 +30,24 @@ const Dialog: React.FC<DialogProps> = ({
     <>
       {showDialog && (
         <div
-          className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end transition-opacity duration-300 z-20 ${
+          className={`fixed inset-0 bg-black bg-opacity-50 flex justify-center items-end transition-opacity duration-300 z-20  ${
             visible ? "opacity-100" : "opacity-0"
           }`}
+          style={{
+            height: "91dvh",
+            // paddingBottom: "",
+          }}
           onClick={onClose}
         >
           <div
-            className={`bg-white rounded-t-lg shadow-lg w-full max-w-lg transition-transform duration-300 transform overflow-y-hidden ${
+            className={`bg-white rounded-t-lg shadow-lg w-full max-w-lg transition-transform duration-300 transform overflow-y-hidden -mt-4 ${
               visible ? "translate-y-0" : "translate-y-full"
             }`}
             onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the dialog
-            style={{
-              height: "80dvh",
-              marginBottom: "4rem",
-            }}
+            // style={{
+            //   height: "80dvh",
+            //   paddingBottom: "4rem",
+            // }}
           >
             <div className="sticky top-0 bg-white p-6 rounded-t-lg flex justify-between items-center border-b z-20">
               <h2 className="text-xl font-bold">{title}</h2>
